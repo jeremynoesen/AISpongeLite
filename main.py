c2fbce2bc7d0c0ec2852824e2ca03bfa48667fcd
Jeremy Noesen <jeremynoesen@gmail.com>
2025-06-01 01:23:41 -0500

    simplify autocomplete logic

--- main.py
+++ main.py
@@ -304,7 +304,7 @@ async def episode(inter: discord.Interaction, topic: str):
 
 
 async def character_autocomplete(interaction: discord.Interaction, current: str,):
-    return [app_commands.Choice(name=character, value=character) for character in [character.title().replace("bob", "Bob") for character in characters.keys() if character != "all" and current.lower() in character]]
+    return [app_commands.Choice(name=character.title().replace("bob", "Bob"), value=character) for character in characters.keys() if character != "all" and current.lower() in character]
 
 
 @command_tree.command(description="Chat with a character.")
