a82fb0c35a80f6cfaebdde401fba0f2f15995fee
Jeremy Noesen <jeremynoesen@gmail.com>
2026-05-09 18:53:19 -0500

    fix gain distortion

--- cogs/standard.py
+++ cogs/standard.py
@@ -425,17 +425,17 @@ class Standard(GroupCog, name="standard", description="Generate episodes, TTS, a
                 # Add lightning if rain is intense
                 if rain_intensity > 0:
                     for i in range((ceil(len(combined) / 1000) if chaos else ceil(min(line_count, 25) / 5)) + rain_intensity):
-                        combined = combined.overlay(sfx_lightning, randrange(len(combined)), gain_during_overlay=((gain_sfx + randint(-10 + rain_intensity, 0)) - sfx_lightning.dBFS))
+                        combined = combined.overlay(sfx_lightning.apply_gain((gain_sfx + randint(-10 + rain_intensity, 0)) - sfx_lightning.dBFS), randrange(len(combined)))
 
             # Add word-activated SFX to the episode
             for sfx in sfx_triggered.keys():
                 for position in sfx_positions[sfx]:
                     variant = choice(sfx_triggered[sfx][0])
-                    combined = combined.overlay(variant, position, gain_during_overlay=((gain_sfx + randint(-10, 0)) - variant.dBFS))
+                    combined = combined.overlay(variant.apply_gain((gain_sfx + randint(-10, 0)) - variant.dBFS), position)
 
             # Add random SFX to the episode
             for sfx in choices(list(sfx_random.keys()), list(sfx_random.values()), k=(ceil(len(combined) / 1000) if chaos else ceil(min(line_count, 25) / 5))):
-                combined = combined.overlay(sfx, randrange(len(combined)), gain_during_overlay=((gain_sfx + randint(-5, 5)) - sfx.dBFS))
+                combined = combined.overlay(sfx.apply_gain((gain_sfx + randint(-5, 5)) - sfx.dBFS), randrange(len(combined)))
 
             # Add the transition SFX to the beginning of the episode
             combined = silence_intro.append(combined, 0).overlay(transition)
